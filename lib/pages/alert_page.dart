d2ee3828c9866ba8a3f0732047df10b86ddd3a92
202102187e <202102187e@utea.edu.pe>
2025-01-13 07:20:16 -0500

    alert page

--- lib/pages/alert_page.dart
+++ lib/pages/alert_page.dart
@@ -1,14 +1,27 @@
-
 import 'package:flutter/material.dart';
 
 class AlertPage extends StatelessWidget {
   const AlertPage({super.key});
+  showMyAlert1(BuildContext context) {
+    showDialog(context: context, builder: (BuildContext context){return Text("Alert 1");});
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("AAlert Page"),
+        title: const Text(
+          "Alert Page",
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.amber,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [ElevatedButton(onPressed: () {showMyAlert1(context);}, child: Text("Alert 1"))],
+        ),
       ),
     );
   }
