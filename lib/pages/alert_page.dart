a2be7ab10ecb8f57a8306bca88af2f3c77de7861
202102187e <202102187e@utea.edu.pe>
2025-01-15 22:13:49 -0500

    ALERT_PAGE

--- lib/pages/alert_page.dart
+++ lib/pages/alert_page.dart
@@ -2,25 +2,156 @@ import 'package:flutter/material.dart';
 
 class AlertPage extends StatelessWidget {
   const AlertPage({super.key});
-  showMyAlert1(BuildContext context) {
-    showDialog(context: context, builder: (BuildContext context){return Text("Alert 1");});
+
+  // Alert 1
+  void showMyAlert1(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Alert 1"),
+          content: Text("Hola este es el contenido del alert1"),
+          backgroundColor: Colors.white,
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text("Cancelar"),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text("Aceptar"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  // Alert 2
+  void showMyAlert2(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Invite People to this Project"),
+          content: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  "Invite existing team members or add new ones.",
+                  style: TextStyle(color: Colors.blueGrey),
+                ),
+                SizedBox(height: 20),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Search team members...',
+                    prefixIcon: Icon(Icons.search), // Ícono de búsqueda
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 10),
+                ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        'https://cdn.pixabay.com/photo/2024/07/22/17/11/elegance-in-profile-8913207_640.png'),
+                  ),
+                  title: Text('Whitney Blessing'),
+                  subtitle: Text('whitneyblessing@gmail.com'),
+                  trailing: ElevatedButton(
+                    onPressed: () {},
+                    child: Text('Add'),
+                  ),
+                ),
+                ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTumr-ynIsoOEzb29PmGsYo5Dfem9Yio8dC8Q&s'),
+                  ),
+                  title: Text('Cheryl Green'),
+                  subtitle: Text('cherylgreen@gmail.com'),
+                  trailing: ElevatedButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Remove',
+                      style: TextStyle(color: Colors.red), // Texto rojo
+                    ),
+                  ),
+                ),
+                ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQBFy0j_72yEKBKNRbPbCzxfNxq1H9Y57ygg&s'),
+                  ),
+                  title: Text('Bonnie Lopez'),
+                  subtitle: Text('bonnielopez@gmail.com'),
+                  trailing: ElevatedButton(
+                    onPressed: () {},
+                    child: Text('Add'),
+                  ),
+                ),
+                SizedBox(height: 20),
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Type their email below...',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Cancel'),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor:
+                    const Color.fromARGB(255, 29, 24, 193), // Fondo azul
+                foregroundColor: Colors.white, // Letras blancas
+              ),
+              onPressed: () {},
+              child: Text('Invite 3 Users'),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          "Alert Page",
-          style: TextStyle(color: Colors.white),
-        ),
+        title: Text("Alert"),
         centerTitle: true,
-        backgroundColor: Colors.amber,
+        backgroundColor: const Color.fromARGB(255, 58, 183, 91),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [ElevatedButton(onPressed: () {showMyAlert1(context);}, child: Text("Alert 1"))],
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                showMyAlert1(context);
+              },
+              child: Text("Alert 1"),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  showMyAlert2(context);
+                },
+                child: Text("Alert 2"),
+              ),
+            ),
+          ],
         ),
       ),
     );
--- lib/pages/card_page.dart
+++ lib/pages/card_page.dart
@@ -7,7 +7,80 @@ class CardPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Card Page"),
+        title: const Text(
+          "Card Page",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.redAccent,
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.all(20.0),
+            padding: EdgeInsets.all(12.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(18.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.2),
+                  offset: Offset(4, 4),
+                  blurRadius: 12.0,
+                ),
+              ],
+            ),
+            child: Column(
+              children: [
+                Text(
+                  "Exercitation eiusmod ad aliquip pariatur nulla aliqua fugiat consequat Lorem. Quis sit ad non cillum enim minim dolor culpa ex Lorem exercitation duis excepteur. Commodo reprehenderit adipisicing sint minim in commodo id fugiat. Quis aliqua aute aliqua tempor ex eu veniam nulla aliqua magna fugiat est. Exercitation do fugiat aliquip consectetur enim. Sint occaecat eiusmod pariatur amet. Nostrud eiusmod elit labore elit elit labore in commodo dolor non irure.Elit in tempor commodo incididunt ullamco dolore eiusmod ut ullamco irure adipisicing cillum et. Eiusmod minim proident proident aliquip ut minim cupidatat officia duis consectetur. Do velit consectetur culpa mollit quis.",
+                  maxLines: 5,
+                  overflow: TextOverflow.ellipsis,
+                  textAlign: TextAlign.center,
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10.0),
+                  padding: EdgeInsets.symmetric(vertical: 10.0),
+                  width: double.infinity,
+                  height: 40.0,
+                  decoration: BoxDecoration(
+                    color: Colors.deepPurpleAccent,
+                    borderRadius: BorderRadius.circular(20.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.deepPurpleAccent,
+                        blurRadius: 10,
+                        offset: Offset(4, 4),
+                      ),
+                    ],
+                  ),
+                  alignment: Alignment.center,
+                  child: Text(
+                    "Follow me",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15.0,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Container(
+            child: Row(
+              children: [
+                Image.network(
+                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYAhBdjd5Dc183RfLQZmtTfkKt34zxUU5iEw&s',
+                ),
+              ],
+              
+            ),
+          )
+        ],
       ),
     );
   }
